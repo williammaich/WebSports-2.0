f8a4acb65ac5a2206723cd7f2df654fedad05b61
Miguel Boanova <mgl.deadly@gmail.com>
2017-09-30 00:14:42 -0300

    modificado layout e fixes

---
+++ resources/Vue/src/components/Authentication/Views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <login
+    api-url="http://localhost:8000"
+    clientId="1"
+    secret="KzvqoGLipYDHrZJSehxBwxDXfhsX2Adyk1THdU8x"
+    @success="handleLogin"
+    @failed="handleErrors">
+    </login>
+</template>
+
+<script>
+import Login from '../Plugins/Login'
+import axios from 'axios'
+
+var instance = axios.create()
+
+export default {
+  components: { Login },
+  methods: {
+    handleLogin (payload) {
+      this.$store.dispatch('setUser', payload.authUser)
+      instance.defaults.headers.common['Authorization'] = payload.headers.Authorization
+    },
+    handleErrors (errors) {
+      alert('Authorization error' + errors)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
-php<?php
+<?php
 
 use Illuminate\Http\Request;
 

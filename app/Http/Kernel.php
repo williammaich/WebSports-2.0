e193e0da61b0b81d9a6bf2cd817be8c21eb43eaa
Miguel Boanova <mgl.deadly@gmail.com>
2017-10-02 17:38:40 -0300

    Arrumado bug CORS e modificado PROCFILE para as novas views

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            'cors',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -59,6 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\CorsMiddleware::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }

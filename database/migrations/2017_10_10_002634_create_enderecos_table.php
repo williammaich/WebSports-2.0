d9e55821d3331b4a65872183971269fb558ee49c
Roger Rezende <javielrezende@gmail.com>
2017-10-09 21:30:09 -0300

    Nova atualização de ER, Criacao da model e migration de enderecos

---
+++ database/migrations/2017_10_10_002634_create_enderecos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('rua', 120);
+            $table->integer('numero');
+            $table->String('complemento', 60);
+            $table->String('cidade', 45);
+            $table->char('cep', 9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}

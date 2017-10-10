bb7be2f27397d02e2a2e08f677feb625565313ba
Roger Rezende <javielrezende@gmail.com>
2017-10-09 21:24:28 -0300

    Adicao de model e migration dos usuarios

---
+++ database/migrations/2017_10_09_222338_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('nome',150);
+            $table->String('senha',32);
+            $table->String('email',120);
+            $table->String('cpf',11);
+            $table->double('saldo',9, 2);
+            $table->String('tipo',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}

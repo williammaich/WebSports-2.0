e6763b55a60269a36402685b1f6556b8596f27a2
Roger Rezende <javielrezende@gmail.com>
2017-10-11 00:27:38 -0300

    Liberação de campos para alteração nas tabelas, implementacao de um seeder para teste e configuracao de timezone brasileira

--- app/Endereco.php
+++ app/Endereco.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Endereco extends Model
 {
-    //
+    protected $fillable = array('rua', 'numero', 'complemento', 'cidade', 'cep');
 }
---
+++ database/seeds/EnderecosTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EnderecosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('enderecos')->insert([
+            'rua' => 'Olavo Bilac',
+            'numero' => 718,
+            'complemento' => 'Casa 7',
+            'cidade' => 'Pelotas',
+            'cep' => 96470000,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s')
+        ]);
+    }
+}

c1dd1e1aeb8750cd951d727d37946b6e14d15371
Roger Rezende <javielrezende@gmail.com>
2017-10-11 11:44:01 -0300

    Seeder de teste para quadras

--- app/Quadra.php
+++ app/Quadra.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quadra extends Model
 {
-    //
+    protected $fillable = array('valorLocacao', 'tipoQuadra_id');
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         //$this->call(UsersTableSeeder::class);
         $this->call(EnderecosTableSeeder::class);
         //$this->call(PagamentosTableSeeder::class);
-        //$this->call(QuadrasTableSeeder::class);
+        $this->call(QuadrasTableSeeder::class);
         //$this->call(ReservasTableSeeder::class);
         $this->call(TipoQuadrasTableSeeder::class);
         //$this->call(UsuariosTableSeeder::class);

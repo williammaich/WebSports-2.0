b75cffd2781f658c4371115d7b4683e7148c5d1c
Miguel Boanova <miguel.boanova@prodigious.com>
2017-11-28 16:49:01 -0200

    Adicionado testes de autenticação

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'password' => \Hash::make('secret'),
+        'remember_token' => '',
     ];
 });

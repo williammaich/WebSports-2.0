43b84c58126285abb73ba2bd5ef76cf6b0d78e93
Roger Rezende <javielrezende@gmail.com>
2017-11-30 13:11:46 -0200

    Fechamento de uma chave na model reserva

--- app/Reserva.php
+++ app/Reserva.php
@@ -8,16 +8,20 @@ class Reserva extends Model
 {
     protected $fillable = array('dataReservada', 'quantidade', 'quadra_id', 'pagamento_id', 'cliente_id');
 
-    public function cliente(){
+    public function cliente()
+    {
         return $this->belongsTo('App\Cliente');
     }
 
-    public function pagamento(){
+    public function pagamento()
+    {
         return $this->belongsTo('App\Pagamento');
     }
 
-    public function quadra(){
+    public function quadra()
+    {
         return $this->belongsTo('App\Quadra');
     }
 
 
+}

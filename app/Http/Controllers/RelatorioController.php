7ff85c11ee11da559e93b1cef3931b1509444982
Miguel Boanova <mgl.deadly@gmail.com>
2017-12-04 19:15:56 -0200

    Fix relatorio

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -7,10 +7,26 @@ use Illuminate\Support\Facades\DB;
 
 class RelatorioController extends Controller
 {
-    public function index()
-    {
-        $users = DB::table('users')->count();
+  public function index()
+  {
+    // Pq não assim?
+    // DB::table não se precisa usar quando temos as models
+    // podemos utilizar chamando direto a model como fiz abaixo
+    // $users = DB::table('users')->count();
 
-        return $users->toJson();
-    }
+    $clientes = \App\Cliente::all()->count();
+    $reservas = \App\Reserva::all()->count();
+
+    // Pq não "->toJson()"
+    // pq o returno do count é só um numero,
+    // ele não consegue transformar um numero sozinho em json
+    // return $clientes->toJson();
+
+
+    // Então criamos um array e transformamos em json
+    return json_encode(array(
+      'clientes' => $clientes,
+      'reservas' => $reservas
+    ));
+  }
 }

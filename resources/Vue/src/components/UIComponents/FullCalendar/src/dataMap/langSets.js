36dd68e67b2a655d8d65cd699728f4c1a454bc3f
Miguel Boanova <mgl.deadly@gmail.com>
2017-12-07 20:29:34 -0200

    removido front

--- resources/Vue/src/components/UIComponents/FullCalendar/src/dataMap/langSets.js
+++
@@ -1,7 +0,0 @@
-export default {
-  en : {
-    weekNames   : ['Dom', 'Seg','Ter','Qua','Qui','Sex','Sáb'],
-    monthNames  : ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-    titleFormat : 'MMMM yyyy'
-  },
-}
